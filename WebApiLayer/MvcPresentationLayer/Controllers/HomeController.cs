5befeb5161338587137050c30d0dfd1e52eb7854
iammetintekin <meto09.mt@gmail.com>
2021-05-02 23:31:40 +0300

    first commit

---
+++ WebApiLayer/MvcPresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPresentationLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<Employee> employees = GetEmployee();
+            return View(employees);
+        }
+
+        public ActionResult Get(int id)
+        {
+            Employee employee = GetEmployeeByID(id);
+            List<Employee> employees = new List<Employee>();
+            employees.Add(employee);
+            
+
+            return View("Index",employees);
+        }
+
+        Employee GetEmployeeByID(int id)
+        {
+            Employee employee = null;
+            using(HttpClient client = new HttpClient())
+            {
+                string url = "http://localhost:9111/api/Employee?id="+id;
+                Task<HttpResponseMessage> result = client.GetAsync(url);
+                if (result.Result.IsSuccessStatusCode)
+                {
+                    Task<string> serializedResult = result.Result.Content.ReadAsStringAsync();
+                    employee = Newtonsoft.Json.JsonConvert.DeserializeObject<Employee>(serializedResult.Result);
+                }
+            }
+            return employee;
+        }
+
+        IEnumerable<Employee> GetEmployee()
+        {
+            IEnumerable<Employee> employees = null;
+            using(HttpClient client = new HttpClient())
+            {
+                string url = "http://localhost:9111/api/Employee";
+                Uri uri = new Uri(url);
+                Task<HttpResponseMessage> result = client.GetAsync(url);
+                if (result.Result.IsSuccessStatusCode)
+                {
+                    Task<string> response = result.Result.Content.ReadAsStringAsync();
+                    employees = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Employee>>(response.Result);
+                }
+            }
+            return employees;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApiLayer/WebApiLayer/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiLayer.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        //http://localhost:9111/api/Employee
+
+        List<Employee> employees;
+        public EmployeeController()
+        {
+            employees = new List<Employee>();
+            employees.Add(new Employee
+            {
+                Id = 1,
+                Name = "Metin",
+                ContactNumber = 6546455,
+                Address = "Aydın"
+            });
+            employees.Add(new Employee
+            {
+                Id = 2,
+                Name = "Servet",
+                ContactNumber = 2135666,
+                Address = "İzmir"
+            });
+        }
+        // GET api/<controller>
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+        // GET api/<controller>/5
+        //http://localhost:9111/api/Employee/1
+        public Employee Get(int id)
+        {
+            return employees.FirstOrDefault<Employee>(x=>x.Id.Equals(id));
+        }
+
+    }
+}
